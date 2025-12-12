3029cd2dbc48ee46aeda75d18c4e79471c07486e
staverm <mauro.staver@gmail.com>
2025-12-12 21:06:50 +0100

    Implement simple agent

--- src/server.py
+++ src/server.py
@@ -20,26 +20,15 @@ def main():
     parser.add_argument("--card-url", type=str, help="URL to advertise in the agent card")
     args = parser.parse_args()
 
-    # Fill in your agent card
-    # See: https://a2a-protocol.org/latest/tutorials/python/3-agent-skills-and-card/
-    
-    skill = AgentSkill(
-        id="",
-        name="",
-        description="",
-        tags=[""],
-        examples=[""]
-    )
-
     agent_card = AgentCard(
-        name="",
-        description="",
+        name="SimpleAgent",
+        description='Responds to messages.',
         url=args.card_url or f"http://{args.host}:{args.port}/",
         version='1.0.0',
         default_input_modes=['text'],
         default_output_modes=['text'],
         capabilities=AgentCapabilities(streaming=True),
-        skills=[skill]
+        skills=[]
     )
 
     request_handler = DefaultRequestHandler(
